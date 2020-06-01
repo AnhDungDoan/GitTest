8c2d9d8fa013e59a5230627e4864c4383a2d5ba1
AnhDungDoan <khongphaidoandung@gmail.com>
2020-06-01 23:14:42 +0700

    Added Point.h

--- GitTest/Vector2.cpp
+++ GitTest/Vector2.cpp
@@ -1,9 +1,12 @@
 #include "Vector2.h"
 
+
 #include <cmath>
 
 double Vector2::norm()
 {
 	return sqrt(x * x + y * y);
 }
+
+
 
--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+#include "Point.h"
 #include "Vector2.h"
 
 int main()
 {
 	std::cout << "Hello World!";
+
+	Point A;
+	Point B;
+
 	return 0;
 }
